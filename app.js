8114e0b5a250bda03b7bbf6062e5f50bdd90f4d4
Christian Franco <svergara1073@gmail.com>
2025-01-27 21:12:14 -0500

    borrar importacion get

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 
 // 1. IMPORTAR LAS DEPENDENCIAS Y MÓDULOS QUE NECESITAMOS
-import express from 'express'; 
-import dotenv from 'dotenv'; 
+import express from 'express';
+import dotenv from 'dotenv';
 import { connectionMongo } from './src/config/dataBase.js';
 import usersRouter from './src/routes/user.routes.js';
 import loginRouter from './src/routes/login.routes.js';
-import cors from 'cors'; 
-import { get } from 'mongoose';
+import cors from 'cors';
 
 // 2. configurar el uso de nuestro servidor y dependencias
-const app = express(); 
-dotenv.config(); 
+const app = express();
+dotenv.config();
 connectionMongo();
-app.use(cors()); 
+app.use(cors());
 
 // Le indico las rutas que debe utilizar
 app.use(express.json());
 app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
-app.get("/", (req,res)=>{
-res.send("backend funcionando correctamente");
+app.get("/", (req, res) => {
+    res.send("backend funcionando correctamente");
 });
 
 export default app;
