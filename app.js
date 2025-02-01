3c5537d1a4d73525c3bfe317f5a6ffe3f2d7af99
Christian Franco <svergara1073@gmail.com>
2025-01-31 19:05:37 -0500

    modificacion carpeta app.js

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 
 // 1. IMPORTAR LAS DEPENDENCIAS Y MÓDULOS QUE NECESITAMOS
+
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectionMongo } from './src/config/dataBase.js';
 import usersRouter from './src/routes/user.routes.js';
 import loginRouter from './src/routes/login.routes.js';
 import cors from 'cors';
+import path from "path";//configurar el acceso al front
+import { fileURLToPath } from "url";
 
 // 2. configurar el uso de nuestro servidor y dependencias
 const app = express();
 dotenv.config();
 connectionMongo();
 app.use(cors());
+//configuraciones para acceder al front
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Le indico las rutas que debe utilizar
 app.use(express.json());
 app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
+
+//vamos a hacer la peticion para quye se muestre nuestro front
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
 app.get("/", (req, res) => {
-    res.send("backend funcionando correctamente");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 export default app;
